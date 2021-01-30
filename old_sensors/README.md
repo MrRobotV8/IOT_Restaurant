f344ab4a41965bebaaec9da274a9629691d96c12
Francesco <francescoguaianav8@outlook.it>
2021-01-30 17:23:34 +0100

    Deleting useless

--- old_sensors/README.md
+++
@@ -1,2 +0,0 @@
-# 20190401_sensor_iot
-Help people interested in this repository understand your project by adding a README.
--- old_sensors/RPi_core_temperature.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def RPi_core_temperature():
-        temp = os.popen("vcgencmd measure_temp").readline()
-        # temp = temp.replace("temp=", "")
-        temp = temp[5:9]
-        return temp
